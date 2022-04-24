4b941b056a5d484ce4a6fb7bfaacf86e2b6d290f
hanseul-Choi <58256559+hanseul-Choi@users.noreply.github.com>
2022-04-25 00:27:24 +0900

    [feature] Compose DropDownMenu

--- app/src/main/java/com/izone/musicplayer/widget/MainScreen.kt
+++ app/src/main/java/com/izone/musicplayer/widget/MainScreen.kt
@@ -1,85 +1,78 @@
 package com.izone.musicplayer.widget
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-val list = listOf("1","2","3")
-
 @Composable
 fun MainScreen() {
+    val itemList = listOf("IZONE", "OhMyGirl", "BTS")
     var expanded by remember { mutableStateOf(false) }
     var title by remember { mutableStateOf("IZONE") }
 
     Box(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.TopStart)) {
-        Row(
-            modifier = Modifier
-                .clickable {
-                    expanded = true
-                }
-                .fillMaxWidth()
-                .padding(15.dp)
-        ){
-            Text(
-                text = title,
-                textAlign = TextAlign.Center,
-                fontSize = 11.sp
-            )
-            Icon(
-                imageVector = Icons.Default.ArrowDropDown,
-                contentDescription = "drop down"
+        .fillMaxSize()) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .clickable {
+                        expanded = true
+                    }
+                    .fillMaxWidth()
+                    .padding(start = 15.dp, end = 15.dp, top = 15.dp, bottom = 10.dp),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = title,
+                    textAlign = TextAlign.Center,
+                    fontSize = 13.sp,
+                    modifier = Modifier.padding(end = 15.dp)
                 )
-        }
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = "drop down"
+                )
+            }
 
-//        Button(
-//            modifier = Modifier.fillMaxWidth(),
-//            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
-//            onClick = { expanded = true }
-//        ) {
-//            Text(
-//                text = title,
-//                textAlign = TextAlign.Center,
-//                fontSize = 11.sp
-//            )
-//        }
-//        DropdownMenu(
-//            expanded = expanded,
-//            onDismissRequest = { expanded = false }
-//        ) {
-//            DropdownMenuItem(onClick = {
-//                title = "IZONE"
-//                expanded = false}) {
-//                Text("IZONE")
-//            }
-//            Divider()
-//            DropdownMenuItem(onClick = {
-//                title = "OhMyGirl"
-//                expanded = false
-//            }) {
-//                Text("OhMyGirl")
-//            }
-//            Divider()
-//            DropdownMenuItem(
-//                onClick = {
-//                    title = "BTS"
-//                    expanded = false
-//                }) {
-//                Text("BTS")
-//            }
-//        }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                itemList.forEach { item ->
+                    DropDownList(title = item) {
+                        title = item
+                        expanded = false
+                    }
+                    Divider()
+                }
+            }
+
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun DropDownList(title: String, clickEvent: () -> Unit) {
+    DropdownMenuItem(
+        onClick = clickEvent) {
+        Text(
+            text = title,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 12.sp
+        )
     }
 }
 
