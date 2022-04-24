4489f9fb221754c7cf0c43473c9f53328b6612e4
hanseuk-Choi <hschoi5542@gmail.com>
2022-04-24 16:57:24 +0900

    [add] compose init

--- app/src/main/java/com/izone/musicplayer/widget/MainScreen.kt
+++ app/src/main/java/com/izone/musicplayer/widget/MainScreen.kt
@@ -2,10 +2,17 @@ package com.izone.musicplayer.widget
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MainScreen() {
     Text(
         text = "Hello"
     )
+}
+
+@Preview(showBackground = true, widthDp = 300, heightDp = 500)
+@Composable
+fun MainPreview() {
+    MainScreen()
 }
